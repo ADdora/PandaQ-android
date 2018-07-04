6e9994f229e5047a3a0d16f0375298151689fedf
Mohamed Morsi <mohamedmorsi9595@gmail.com>
2018-07-04 03:01:26 +0300

    first push

---
+++ app/src/main/java/com/mohamedmorsi/pandaq/Logo.java
@@ -0,0 +1,13 @@
+package com.mohamedmorsi.pandaq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Logo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
